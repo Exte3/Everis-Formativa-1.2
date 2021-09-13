f66198bd37d21915344c7960182681f2ce314fcd
Hector Gabriel <nunkmeconecto@gmail.com>
2021-09-13 14:19:02 -0300

    Commit Inicial

---
+++ src/main/java/com/everis/formativa/EverisFormativa13Application.java
@@ -0,0 +1,13 @@
+package com.everis.formativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverisFormativa13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverisFormativa13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/formativa/EverisFormativa13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.formativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverisFormativa13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
